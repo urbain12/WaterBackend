de50e24d87cb38d34e1a668d2f30a7771fd6b067
urbain12 <kortenyurbain@gmail.com>
2021-09-25 15:33:04 +0200

    chnage

--- users/views.py
+++ users/views.py
@@ -1758,7 +1758,7 @@ class register(CreateAPIView):
                 customer.Sector = request.data['Sector']
                 customer.Cell = request.data['Cell']
                 customer.Language = request.data['Language']
-                customer.Image = request.data['Image']
+                customer.Image = request.FILES['Image']
                 customer.save()
                 response = {
                     'status': 'success',
